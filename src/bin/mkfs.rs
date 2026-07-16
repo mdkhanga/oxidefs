a19e3f46457bac428a63a3aba324bdf3fa36216c
mdkhanga <khangaonkar@gmail.com>
2026-07-16 07:11:37 -0700

    getting back to it

---
+++ src/bin/mkfs.rs
@@ -0,0 +1,8 @@
+//! mkfs.oxidefs - Creates a new OxideFS filesystem image
+//!
+//! Usage: mkfs.oxidefs <image_path> <size>
+//! Example: mkfs.oxidefs myfs.img 100M
+
+fn main() {
+    println!("mkfs.oxidefs - not yet implemented");
+}
---
+++ src/bin/mount.rs
@@ -0,0 +1,8 @@
+//! mount.oxidefs - Mounts an OxideFS filesystem
+//!
+//! Usage: mount.oxidefs <image_path> <mount_point>
+//! Example: mount.oxidefs myfs.img /mnt/oxidefs
+
+fn main() {
+    println!("mount.oxidefs - not yet implemented");
+}
